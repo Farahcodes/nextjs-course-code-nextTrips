de0bbb242ad7f51340f66781941f9add90ed6263
farah <farah.fournat@gmail.com>
2023-02-24 20:41:41 +0100

    fetching all meetups in getstatic props

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -35,12 +37,27 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   //fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://ffournat:ZhkvPLEuJQWKO0KG@cluster0.tqeeu2x.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     // making sure to update the page after deployment
-    revalidate: 10,
+    revalidate: 1,
   };
 }
 

b47b63fd23ee402bf9455240e4db3bcb72485ba7
farah <farah.fournat@gmail.com>
2023-02-24 20:55:11 +0100

    generate dynamic id

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
@@ -12,20 +14,21 @@ function MeetupDetails() {
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://ffournat:ZhkvPLEuJQWKO0KG@cluster0.tqeeu2x.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 

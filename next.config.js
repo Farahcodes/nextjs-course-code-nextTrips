08731fc999bfccf7ff94a4199212f5ed38d11678
farah <farah.fournat@gmail.com>
2023-02-26 18:26:57 +0100

    update dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      fs: false,
-      module: false,
-    };
+// module.exports = {
+//   webpack: (config) => {
+//     config.resolve.fallback = {
+//       fs: false,
+//       module: false,
+//     };
 
-    return config;
-  },
-};
+//     return config;
+//   },
+// };
